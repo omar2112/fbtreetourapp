916e4832b9ff5a92ed83a7f54c31a7c546940690
bluefox <you@example.com>
2016-04-19 01:13:21 -0700

    added a home screen and about screen. That's 2 functionalities, right? :D

--- app/src/main/java/com/example/bluefox/listtest1/MainActivity.java
+++ app/src/main/java/com/example/bluefox/listtest1/MainActivity.java
@@ -37,46 +37,33 @@ public class MainActivity extends AppCompatActivity {
 
         String[] homeOptions = {"Home", "Tree Tour", "Tree Directory", "Favorites",
                                 "About", "Contact Us", "Help"};
-
-        String[] treeDir = {"Deodar Cedar", "Monterey Pine", "European Larch", "China-Fir",
-                            "Japanese Snowbell Tree", "Coast Redwood", "Bigcone Pine",
-                            "Lawson Cypress", "English Elm", "Pin Oak", "Eastern Dogwood"};
-        /*
-        String[] sciDir = {"Cedrus Deodara, Cornus Nuttallii", "Pinus radiata",
-                           "Larix decidua", "Cunninghamia lanceolata"}; */
         ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                treeDir);
-        ListView theListView = (ListView) findViewById(R.id.theListView);
+                homeOptions);
+        ListView mainMenuView = (ListView) findViewById(R.id.mainMenuView);
 
-        theListView.setAdapter(theAdapter);
+        mainMenuView.setAdapter(theAdapter);
 
-        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mainMenuView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                /*
-                String treePicked = "You selected " +
-                        String.valueOf(adapterView.getItemAtPosition(i));
 
-                Toast.makeText(MainActivity.this, treePicked, Toast.LENGTH_SHORT).show();
-                */
-                //Class<TreeinfoActivity> switchTreeClass = TreeinfoActivity.class;
                 Intent changeScreen;
-                if (i == 0) {
-                    changeScreen = new Intent(view.getContext(), TreeinfoActivity.class);
-                } else if (i == 1) {
-                    changeScreen = new Intent(view.getContext(), MontereyPineActivity.class);
-                } else if (i == 2) {
-                    changeScreen = new Intent(view.getContext(), EuropeanLarchActivity.class);
+                if (i == 4) {
+                    changeScreen = new Intent(view.getContext(), AboutActivity.class);
                 } else {
                     changeScreen = new Intent(view.getContext(), TreeinfoActivity.class);
+
                 }
 
-                //Intent changeScreen = new Intent(view.getContext(), TreeinfoActivity.class);
-                //Intent changeScreen = new Intent(view.getContext(), switchTreeClass);
-                startActivityForResult(changeScreen, 0);
 
+
+                startActivityForResult(changeScreen, 0);
             }
         });
 
+
     }
+     //   });
+
+
 }
--- app/src/main/java/com/example/bluefox/listtest1/TreeinfoActivity.java
+++ app/src/main/java/com/example/bluefox/listtest1/TreeinfoActivity.java
@@ -3,6 +3,11 @@ package com.example.bluefox.listtest1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class TreeinfoActivity extends AppCompatActivity {
 
@@ -10,5 +15,44 @@ public class TreeinfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treeinfo);
+        String[] treeDir = {"Deodar Cedar", "Monterey Pine", "European Larch", "China-Fir",
+                "Japanese Snowbell Tree", "Coast Redwood", "Bigcone Pine",
+                "Lawson Cypress", "English Elm", "Pin Oak", "Eastern Dogwood", "Ginkgo",
+                "American White Elm", "Honey Locust", "Silk Tree", "Pindrow Fir", "Cork Oak"};
+        /*
+        String[] sciDir = {"Cedrus Deodara, Cornus Nuttallii", "Pinus radiata",
+                           "Larix decidua", "Cunninghamia lanceolata"}; */
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                treeDir);
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                /*
+                String treePicked = "You selected " +
+                        String.valueOf(adapterView.getItemAtPosition(i));
+
+                Toast.makeText(MainActivity.this, treePicked, Toast.LENGTH_SHORT).show();
+                */
+                //Class<TreeinfoActivity> switchTreeClass = TreeinfoActivity.class;
+                Intent changeScreen;
+                if (i == 0) {
+                    changeScreen = new Intent(view.getContext(), DeodarCedarActivity.class);
+                } else if (i == 1) {
+                    changeScreen = new Intent(view.getContext(), MontereyPineActivity.class);
+                } else if (i == 2) {
+                    changeScreen = new Intent(view.getContext(), EuropeanLarchActivity.class);
+                } else {
+                    changeScreen = new Intent(view.getContext(), DeodarCedarActivity.class);
+                }
+
+                //Intent changeScreen = new Intent(view.getContext(), TreeinfoActivity.class);
+                //Intent changeScreen = new Intent(view.getContext(), switchTreeClass);
+                startActivityForResult(changeScreen, 0);
+            }
+        });
     }
 }
